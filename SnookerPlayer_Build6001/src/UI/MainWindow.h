696fe1d6f7d1237069f4634bb781a781c0105c0c
Andre erick <andre.erick@gmail.com>
2026-08-16 21:34:18 +0200

    Enhance MainWindow with UI components and method
    Added private members for UI components and a refreshDisplay method.

--- SnookerPlayer_Build6001/src/UI/MainWindow.h
+++ SnookerPlayer_Build6001/src/UI/MainWindow.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QFrame>
+#include "GameManager.h"
 
 class MainWindow : public QMainWindow
 {
@@ -8,4 +11,17 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget* parent = nullptr);
+
+private:
+    void refreshDisplay();
+
+    GameManager m_gameManager;
+
+    QFrame* m_player1Frame = nullptr;
+    QFrame* m_player2Frame = nullptr;
+
+    QLabel* m_player1Label = nullptr;
+    QLabel* m_player2Label = nullptr;
+    QLabel* m_redsLabel = nullptr;
+    QLabel* m_phaseLabel = nullptr;
 };
